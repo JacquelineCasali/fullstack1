1f09447f80105ef03692fcd4fd04d454250b7e1b
Jacqueline <jcasalia@yahoo.com.br>
2025-09-09 16:30:33 -0300

    criando o post e o domain

---
+++ jtech-tasklist-backend/src/main/java/br/com/jtech/tasklist/domain/Task.java
@@ -0,0 +1,37 @@
+package br.com.jtech.tasklist.model;
+
+import jakarta.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Entity
+@Data
+public class TaskModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+
+    @Column(length = 50)
+    private String titulo;
+    private String descricao;
+    private String status; // ex: pendente, concluída
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitulo (String titulo) throws Exception {
+        if (titulo.length() > 50) {
+            throw new Exception("O campo titulo deve conter no máximo 50 caracteres");
+        }
+        this.titulo = titulo;
+    }
+
+}
